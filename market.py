00044cedc1466083aa8f20dc50fbbe377bf49ee0
aminaamin0211 <aminaamin0211@gmail.com>
2022-10-16 02:21:55 +0100

    update to main python file

--- market.py
+++ market.py
@@ -2,8 +2,26 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def home_page():
     return render_template('home.html')
+
+@app.route('/products')
+def products_page():
+    items = [
+        {'productID': 1, 'item_name': 'Cerave Hydrating Cleanser with Hyaluronic Acid', 'quantity': '50', 'price': '9.99', 'Location': 'North London'},
+        {'productID': 2, 'item_name': 'Cerave Hydrating Cleanser with Hyaluronic Acid', 'quantity': '50',
+         'price': '9.99', 'Location': 'North London'},
+        {'productID': 3, 'item_name': 'Cerave Hydrating Cleanser with Hyaluronic Acid', 'quantity': '50',
+         'price': '9.99', 'Location': 'North London'},
+        {'productID': 4, 'item_name': 'Cerave Hydrating Cleanser with Hyaluronic Acid', 'quantity': '50',
+         'price': '9.99', 'Location': 'North London'},
+        {'productID': 5, 'item_name': 'Cerave Hydrating Cleanser with Hyaluronic Acid', 'quantity': '50',
+         'price': '9.99', 'Location': 'North London'},
+    ]
+    return render_template('products.html', items=items)
+
+
 
 
 if __name__ == "__main__":
